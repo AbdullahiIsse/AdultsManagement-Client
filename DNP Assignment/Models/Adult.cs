7dd13413e0aa4e0a5e010352770356a9f4bb69bc
abdullahiisse <abdullahi12351@gmail.com>
2021-10-10 20:08:10 +0200

    done

--- DNP Assignment/Models/Adult.cs
+++ DNP Assignment/Models/Adult.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace Models {
 public class Adult : Person {
-    [Required] public Job JobTitle { get; set; }
+     public Job JobTitle { get; set; }
     
     public override string ToString() {
         return JsonSerializer.Serialize(this);
