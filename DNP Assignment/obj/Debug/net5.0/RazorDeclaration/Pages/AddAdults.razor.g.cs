4a030a29fa64ac59e2c29b9c7683fec0db6b0cc4
abdullahiisse <abdullahi12351@gmail.com>
2021-10-31 21:39:00 +0100

    update

--- DNP Assignment/obj/Debug/net5.0/RazorDeclaration/Pages/AddAdults.razor.g.cs
+++ DNP Assignment/obj/Debug/net5.0/RazorDeclaration/Pages/AddAdults.razor.g.cs
@@ -13,91 +13,91 @@ namespace DNP_Assignment.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 1 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 2 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 3 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 4 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 5 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 6 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 7 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 8 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 9 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using DNP_Assignment;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\_Imports.razor"
+#line 10 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\_Imports.razor"
 using DNP_Assignment.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\Pages\AddAdults.razor"
+#line 3 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\Pages\AddAdults.razor"
 using DNP_Assignment.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\Pages\AddAdults.razor"
+#line 4 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\Pages\AddAdults.razor"
 using Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\Pages\AddAdults.razor"
+#line 2 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\Pages\AddAdults.razor"
            [Authorize]
 
 #line default
@@ -112,7 +112,7 @@ using Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 45 "C:\Users\abdul\RiderProjects\AdultBlazor\DNP Assignment\Pages\AddAdults.razor"
+#line 45 "C:\Users\abdul\RiderProjects\DNPAssignmentBlazor\DNP Assignment\Pages\AddAdults.razor"
        
     
     private Adult newAdult = new Adult();
